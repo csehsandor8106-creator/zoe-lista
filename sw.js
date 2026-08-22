56ead58387a0e7785931e38d78e9d44219a40f2d
csehsandor8106-creator <csehsandor8106@gmail.com>
2026-08-22 20:36:16 +0200

    Refresh cache for clothing recognition

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zoe-lista-v9';
+const CACHE='zoe-lista-v10';
 const ASSETS=['./','./index.html','./styles.css','./catalog-data.js','./catalog-patches.js','./input-normalizer.js','./app.js','./update-manager.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
