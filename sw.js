bb49acad42275f53c784dcbdaa92371ec67c9991
csehsandor8106-creator <csehsandor8106@gmail.com>
2026-08-22 20:12:19 +0200

    Cache catalog patches and bump PWA cache to v7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='zoe-lista-v6';
-const ASSETS=['./','./index.html','./styles.css','./catalog-data.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='zoe-lista-v7';
+const ASSETS=['./','./index.html','./styles.css','./catalog-data.js','./catalog-patches.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(resp=>{const clone=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,clone));return resp;}).catch(()=>caches.match('./index.html'))));});
